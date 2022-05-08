806f8f60ea36fdf4996f719b689c780d94e0133a
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-05-08 22:28:22 +0900

    feat: implement CoatingLayer
    Add tests

--- dipcoatimage/finitedepth/tests/test_coatinglayer.py
+++ dipcoatimage/finitedepth/tests/test_coatinglayer.py
@@ -1,5 +1,12 @@
+import cv2  # type: ignore
 from dataclasses import dataclass
-from dipcoatimage.finitedepth import CoatingLayerBase
+from dipcoatimage.finitedepth import (
+    get_samples_path,
+    CoatingLayerBase,
+    SubstrateReference,
+    Substrate,
+    LayerArea,
+)
 import pytest
 
 
@@ -167,3 +174,32 @@ def test_invalid_subclassing():
                 pass
 
             Data = type
+
+
+def test_CoatingLayer_template_matching():
+    ref_img = cv2.imread(get_samples_path("ref1.png"))
+    ref = SubstrateReference(ref_img, (200, 50, 1200, 200), (400, 100, 1000, 500))
+    subst = Substrate(ref)
+    coat_img = cv2.imread(get_samples_path("coat1.png"))
+    coat = LayerArea(coat_img, subst)
+
+    assert coat.template_point() == (200, 501)
+    assert coat.substrate_point() == (400, 551)
+
+
+def test_CoatingLayer_capbridge_broken():
+    ref_img = cv2.imread(get_samples_path("ref3.png"))
+    ref = SubstrateReference(ref_img, (100, 50, 1200, 600), (200, 50, 1100, 800))
+    subst = Substrate(ref)
+
+    vid_path = get_samples_path("coat3.mp4")
+    cap = cv2.VideoCapture(vid_path)
+    _, unbroke_img = cap.read()
+    cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_FRAME_COUNT) - 1)
+    _, broke_img = cap.read()
+    cap.release()
+
+    unbroke_layer = LayerArea(unbroke_img, subst)
+    assert not unbroke_layer.capbridge_broken()
+    broke_layer = LayerArea(broke_img, subst)
+    assert broke_layer.capbridge_broken()
