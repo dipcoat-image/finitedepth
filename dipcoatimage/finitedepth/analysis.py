b98da5760ee0fc1651163a88b5a70a77c048ca68
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-05-11 20:11:23 +0900

    feat: implement Experiment
    Implement DataWriter
    Implement CSVWriter
    Implement Analyzer

--- dipcoatimage/finitedepth/analysis.py
+++ dipcoatimage/finitedepth/analysis.py
@@ -14,6 +14,12 @@ Analysis
 
 .. autofunction:: experiment_kind
 
+.. autoclass:: DataWriter
+   :members:
+
+.. autoclass:: CSVWriter
+   :members:
+
 ------------------
 Data serialization
 ------------------
@@ -41,13 +47,17 @@ Data serialization
 
 """
 
+import abc
+import csv
 import cv2  # type: ignore
 import dataclasses
 import enum
 import mimetypes
 import numpy as np
 import numpy.typing as npt
-from typing import List, Type, Optional, Union
+import os
+import tqdm  # type: ignore
+from typing import List, Type, Optional, Union, Dict, Any
 
 from .reference import SubstrateReferenceBase
 from .substrate import SubstrateBase
@@ -59,6 +69,8 @@ from .util import import_variable, data_converter, OptionalROI, DataclassProtoco
 __all__ = [
     "ExperimentKind",
     "experiment_kind",
+    "DataWriter",
+    "CSVWriter",
     "ImportArgs",
     "ReferenceArgs",
     "SubstrateArgs",
@@ -132,6 +144,274 @@ def experiment_kind(paths: List[str]) -> ExperimentKind:
     else:
         ret = ExperimentKind.NullExperiment
     return ret
+
+
+class DataWriter(abc.ABC):
+    """
+    Abstract base class to write data file.
+
+    Parameters
+    ==========
+
+    path
+        Path to the data file.
+
+    headers
+        Headers for the data file.
+
+    """
+
+    def __init__(self, path: str, headers: List[str]):
+        self.path = path
+        self.headers = headers
+
+    @abc.abstractmethod
+    def prepare(self):
+        """Prepare to write the data, e.g. create file or write headers."""
+        pass
+
+    @abc.abstractmethod
+    def write_data(self, data: List[Any]):
+        """Write *data* to the file."""
+        pass
+
+    @abc.abstractmethod
+    def terminate(self):
+        """Terminate the writing and close the file."""
+        pass
+
+
+class CSVWriter(DataWriter):
+    """
+    Writer for CSV file.
+
+    Examples
+    ========
+
+    >>> from dipcoatimage.finitedepth.analysis import CSVWriter
+    >>> writer = CSVWriter("data.csv", ["foo", "bar"])
+    >>> def write_csv():
+    ...     writer.prepare()
+    ...     writer.write_data([10, 20])
+    ...     writer.terminate()
+    >>> write_csv() #doctest: +SKIP
+
+    """
+
+    def prepare(self):
+        self.datafile = open(self.path, "w", newline="")
+        self.writer = csv.writer(self.datafile)
+        self.writer.writerow(self.headers)
+
+    def write_data(self, data: List[Any]):
+        self.writer.writerow(data)
+
+    def terminate(self):
+        self.datafile.close()
+
+
+class Analyzer:
+    """
+    Class to save the analysis result.
+
+    Parameters
+    ==========
+
+    paths
+        Paths to the coated substrate image/video files.
+
+    experiment
+        Experiment instance to analyze the coated substrate images.
+
+    Attributes
+    ==========
+
+    paths, experiment
+
+    datawriters
+        Dictionary of file extensions and their data writers.
+
+    videocodecs
+        Dictionary of video extensions and their FourCC values.
+
+    """
+    def __init__(self, paths: List[str], experiment: ExperimentBase):
+        self.paths = paths
+        self.experiment = experiment
+
+        self.datawriters: Dict[str, Type[DataWriter]] = dict(csv=CSVWriter)
+        self.videocodecs: Dict[str, int] = dict(mp4=cv2.VideoWriter_fourcc(*"mp4v"))
+
+    def analyze(
+        self,
+        data_path: str = "",
+        image_path: str = "",
+        video_path: str = "",
+        *,
+        fps: Union[int, float, None] = None,
+        name: str = "",
+    ):
+        """
+        Analyze :attr:`paths` with :attr:`experiment` and save the result.
+
+        .. rubric:: Analysis data
+
+        If *data_path* is passed, analysis result is saved as data file.
+        Data writer is searched from :attr:`datawriters` using file extension.
+
+        If FPS value is nonzero, time value for each coating layer is
+        automatically prepended to the row.
+
+        .. rubric:: Visualization image
+
+        If *image_path* is passed, visualization results are saved as image
+        files. Using formattable string (e.g. ``img_%02d.jpg``) to save multiple
+        images.
+
+        .. rubric:: Visualization video
+
+        If *video_path* is passed, visualization results are saved as video file.
+        FourCC codec value is searched from :attr:`videocodecs` using file
+        extension.
+
+        .. rubric:: FPS
+
+        If *fps* is explicitly passed, it is used for data timestamps and
+        visualization video FPS.
+
+        .. rubric:: Progress bar
+
+        Progress bar is printed. Pass *name* for the progress bar name.
+
+        """
+        self.experiment.substrate.reference.verify()
+        self.experiment.substrate.verify()
+        self.experiment.verify()
+
+        expt_kind = experiment_kind(self.paths)
+
+        # make image generator
+        if (
+            expt_kind == ExperimentKind.SingleImageExperiment
+            or expt_kind == ExperimentKind.MultiImageExperiment
+        ):
+            img_gen = (cv2.imread(path) for path in self.paths)
+            if fps is None:
+                fps = 0
+            h, w = cv2.imread(self.paths[0]).shape[:2]
+
+            total = len(self.paths)
+
+        elif expt_kind == ExperimentKind.VideoExperiment:
+            (path,) = self.paths
+            cap = cv2.VideoCapture(path)
+            fnum = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            img_gen = (cap.read()[1] for _ in range(fnum))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+            total = fnum
+
+        else:
+            raise TypeError(f"Unsupported experiment kind: {expt_kind}")
+
+        # prepare for data writing
+        if data_path:
+            write_data = True
+
+            dirname, _ = os.path.split(data_path)
+            if dirname:
+                os.makedirs(dirname, exist_ok=True)
+
+            _, data_ext = os.path.splitext(data_path)
+            data_ext = data_ext.lstrip(os.path.extsep).lower()
+            writercls = self.datawriters.get(data_ext, None)
+            if writercls is None:
+                raise TypeError(f"Unsupported extension: {data_ext}")
+            headers = [
+                f.name for f in dataclasses.fields(self.experiment.layer_type.Data)
+            ]
+            if fps:
+                headers = ["time (s)"] + headers
+            datawriter = writercls(data_path, headers)
+            datawriter.prepare()
+        else:
+            write_data = False
+
+        # prepare for image writing
+        if image_path:
+            write_image = True
+
+            dirname, _ = os.path.split(image_path)
+            if dirname:
+                os.makedirs(dirname, exist_ok=True)
+            try:
+                image_path % 0
+                image_path_formattable = True
+            except (TypeError, ValueError):
+                image_path_formattable = False
+        else:
+            write_image = False
+
+        # prepare for video writing
+        if video_path:
+            write_video = True
+
+            _, video_ext = os.path.splitext(video_path)
+            video_ext = video_ext.lstrip(os.path.extsep).lower()
+            fourcc = self.videocodecs.get(video_ext, None)
+            if fourcc is None:
+                raise TypeError(f"Unsupported extension: {video_ext}")
+
+            dirname, _ = os.path.split(video_path)
+            if dirname:
+                os.makedirs(dirname, exist_ok=True)
+            videowriter = cv2.VideoWriter(video_path, fourcc, fps, (w, h))
+        else:
+            write_video = False
+
+        layer_gen = self.experiment.layer_generator()
+        try:
+            for i, img in enumerate(tqdm(img_gen, total=total, desc=name)):
+                if img is None:
+                    continue
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                next(layer_gen)
+                layer = layer_gen.send(img)
+                valid = layer.valid()
+
+                if write_data:
+                    if valid:
+                        data = list(dataclasses.astuple(layer.analyze()))
+                        if fps:
+                            data = [i / fps] + data
+                    else:
+                        data = []
+                    datawriter.write_data(data)
+
+                if write_image or write_video:
+                    if valid:
+                        visualized = layer.draw()
+                    else:
+                        visualized = img
+
+                    if write_image:
+                        if image_path_formattable:
+                            imgpath = image_path % i
+                        else:
+                            imgpath = image_path
+                        cv2.imwrite(imgpath, visualized)
+
+                    if write_video:
+                        videowriter.write(visualized)
+        finally:
+            if write_data:
+                datawriter.terminate()
+
+            if write_video:
+                videowriter.release()
 
 
 @dataclasses.dataclass
