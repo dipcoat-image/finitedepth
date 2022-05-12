9f23ad65c803a06f658c5cf9325a1d7bb1e50f1b
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-05-12 09:08:29 +0900

    feat: implement Experiment
    Add test for CSVWriter

--- dipcoatimage/finitedepth/tests/test_analysis.py
+++ dipcoatimage/finitedepth/tests/test_analysis.py
@@ -1,5 +1,8 @@
+import csv
+import os
+import pytest
 from dipcoatimage.finitedepth import get_samples_path
-from dipcoatimage.finitedepth.analysis import ExperimentKind, experiment_kind
+from dipcoatimage.finitedepth.analysis import ExperimentKind, experiment_kind, CSVWriter
 
 
 def test_experiment_kind():
@@ -33,3 +36,29 @@ def test_experiment_kind():
         get_samples_path("coat1.png"),
     ]
     assert experiment_kind(vidimg_path) == ExperimentKind.NullExperiment
+
+
+def test_CSVWriter(tmp_path):
+    datapath = os.path.join(tmp_path, "data.csv")
+    headers = ["foo", "bar"]
+    row1 = [1, 2]
+    row2 = [3, 4]
+    writer = CSVWriter(datapath, headers)
+    writer.prepare()
+    writer.write_data(row1)
+    writer.write_data(row2)
+    writer.terminate()
+
+    assert os.path.exists(datapath)
+
+    with open(datapath, 'r') as datafile:
+        reader = csv.reader(datafile)
+        data_headers = next(reader)
+        data_row1 = next(reader)
+        data_row2 = next(reader)
+        with pytest.raises(StopIteration):
+            next(reader)
+
+    assert data_headers == headers
+    assert data_row1 == [str(i) for i in row1]
+    assert data_row2 == [str(i) for i in row2]
