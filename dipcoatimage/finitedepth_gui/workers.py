d9444e14e5984b5d243b91ef33a003a6a109be10
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-05-16 15:12:58 +0900

    feat: define more workers
    Define ExperimentWorker

--- dipcoatimage/finitedepth_gui/workers.py
+++ dipcoatimage/finitedepth_gui/workers.py
@@ -7,20 +7,32 @@ control widgets.
 
 """
 
-from dipcoatimage.finitedepth import SubstrateReferenceBase, SubstrateBase
+import cv2  # type: ignore
+from dipcoatimage.finitedepth import (
+    SubstrateReferenceBase,
+    SubstrateBase,
+    CoatingLayerBase,
+    ExperimentBase,
+)
 from dipcoatimage.finitedepth.util import OptionalROI, DataclassProtocol
 import enum
 import numpy as np
 import numpy.typing as npt
 from PySide6.QtCore import QObject, Slot, Signal
-from typing import Optional, Type
-from .controlwidgets import ReferenceWidgetData, SubstrateWidgetData
+from typing import Optional, Type, Generator
+from .controlwidgets import (
+    ReferenceWidgetData,
+    SubstrateWidgetData,
+    CoatingLayerWidgetData,
+    ExperimentWidgetData,
+)
 
 
 __all__ = [
     "ReferenceWorker",
     "SubstrateWorker",
     "ExperimentVisualizationMode",
+    "ExperimentWorker",
 ]
 
 
@@ -281,14 +293,14 @@ class SubstrateWorker(QObject):
         """
         return self._ref
 
-    def parameters(self):
+    def parameters(self) -> Optional[DataclassProtocol]:
         """
         *parameters* for :meth:`substrateType` to construct :meth:`substrate`.
         ``None`` indicates invalid value.
         """
         return self._params
 
-    def drawOptions(self):
+    def drawOptions(self) -> Optional[DataclassProtocol]:
         """
         *draw_options* for :meth:`substrateType` to construct :meth:`substrate`.
         ``None`` indicates invalid value.
@@ -428,6 +440,345 @@ class ExperimentVisualizationMode(enum.IntEnum):
         substrate removal is performed.
 
     """
+
     OFF = 0
     FULL = 1
     FAST = 2
+
+
+class ExperimentWorker(QObject):
+    """
+    Worker to build the concreate instance of :class:`ExperimentBase` and to
+    visualize it.
+
+    Data for experiment object are:
+
+    1. :meth:`experimentType`
+    2. :meth:`substrate`
+    3. :meth:`coatingLayerType`
+    4. :meth:`coatingLayerParameters`
+    5. :meth:`coatingLayerDrawOptions`
+    6. :meth:`coatingLayerDecoOptions`
+    7. :meth:`parameters`
+
+    1st and 7th data are updated by :meth:`setExperimentWidgetData`. 2nd datum is
+    updated by :meth:`setSubstrate`. 3rd, 4th, 5th and 6th data are updated by
+    :meth:`setCoatingLayerWidgetData`.
+
+    :meth:`updateExperiment` constructs the experiment object with data.
+    Resulting object can be acquired by :meth:`experiment`, or calling
+    :meth:`emitExperiment` and listening to :attr:`experimentChanged` signal.
+
+    To visualize the layer shape image, pass it to :meth:`setImage`
+    first. Visualization result can be directly acquired from
+    :meth:`visualizedImage`, or calling :meth:`emitImage` and listening
+    to :attr:`visualizedImageChanged` signal.
+
+    """
+
+    experimentChanged = Signal(object)
+    visualizedImageChanged = Signal(np.ndarray)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initArgs()
+        self._visualize_mode = ExperimentVisualizationMode.full
+
+    def initArgs(self):
+        self._type = None
+        self._subst = None
+        self._layer_type = None
+        self._layer_params = None
+        self._layer_drawopts = None
+        self._layer_decoopts = None
+        self._params = None
+
+        self._expt = None
+        self._layer_generator = None
+
+        self._img = None
+
+    def experimentType(self) -> Optional[Type[ExperimentBase]]:
+        """
+        Type object to construct :meth:`experiment`. ``None`` indicates invalid
+        value.
+        """
+        return self._type
+
+    def substrate(self) -> Optional[SubstrateBase]:
+        """
+        Substrate instance to construct :meth:`experiment`. ``None`` indicates
+        invalid value.
+        """
+        return self._subst
+
+    def coatingLayerType(self) -> Optional[Type[CoatingLayerBase]]:
+        """
+        Coating layer type object to construct :meth:`experiment`. ``None``
+        indicates invalid value.
+        """
+        return self._layer_type
+
+    def coatingLayerParameters(self) -> Optional[DataclassProtocol]:
+        """
+        *parameters* for :meth:`coatingLayerType` to construct
+        :meth:`experiment`. ``None`` indicates invalid value.
+        """
+        return self._layer_params
+
+    def drawOptions(self) -> Optional[DataclassProtocol]:
+        """
+        *draw_options* for :meth:`coatingLayerType` to construct
+        :meth:`experiment`. ``None`` indicates invalid value.
+        """
+        return self._layer_drawopts
+
+    def decoOptions(self) -> Optional[DataclassProtocol]:
+        """
+        *deco_options* for :meth:`coatingLayerType` to construct
+        :meth:`experiment`. ``None`` indicates invalid value.
+        """
+        return self._layer_decoopts
+
+    def parameters(self) -> Optional[DataclassProtocol]:
+        """
+        *parameters* for :meth:`experimentType` to construct :meth:`experiment`.
+        ``None`` indicates invalid value.
+        """
+        return self._params
+
+    def setExperimentWidgetData(self, data: ExperimentWidgetData):
+        """
+        Update following values with *data*.
+
+        1. :meth:`experimentType`
+        2. :meth:`experimentParameters`
+        """
+        expttype = data.type
+        if not (isinstance(expttype, type) and issubclass(expttype, ExperimentBase)):
+            expttype = None
+        self._type = expttype
+
+        params = data.parameters
+        if expttype is None:
+            params = None
+        elif isinstance(params, expttype.Parameters):
+            pass
+        else:
+            try:
+                params = expttype.Parameters()
+            except TypeError:
+                params = None
+        self._params = params
+
+    def setSubstrate(self, subst: Optional[SubstrateBase]):
+        """Update :meth:`substrate` with *subst*."""
+        self._subst = subst
+
+    def setCoatingLayerWidgetData(self, data: CoatingLayerWidgetData):
+        """
+        Update following values with *data*.
+
+        1. :meth:`coatingLayerType`
+        2. :meth:`coatingLayerParameters`
+        3. :meth:`coatingLayerDrawOptions`
+        4. :meth:`coatingLayerDecoOptions`
+        """
+        coattype = data.type
+
+        if not (isinstance(coattype, type) and issubclass(coattype, CoatingLayerBase)):
+            coattype = None
+        self._layer_type = coattype
+
+        coat_params = data.parameters
+        if coattype is None:
+            coat_params = None
+        elif isinstance(coat_params, coattype.Parameters):
+            pass
+        else:
+            try:
+                coat_params = coattype.Parameters()
+            except TypeError:
+                coat_params = None
+        self._layer_params = coat_params
+
+        coat_drawopts = data.draw_options
+        if coattype is None:
+            coat_drawopts = None
+        elif isinstance(coat_drawopts, coattype.DrawOptions):
+            pass
+        else:
+            try:
+                coat_drawopts = coattype.DrawOptions()
+            except TypeError:
+                coat_drawopts = None
+        self._layer_drawopts = coat_drawopts
+
+    def updateExperiment(self):
+        """Update :meth:`experiment` and :meth:`layerGenerator`."""
+        expt = None
+
+        default_invalid_args = [
+            self.experimentType(),
+            self.substrate(),
+            self.coatingLayerType(),
+            self.coatingLayerParameters(),
+            self.coatingLayerDrawOptions(),
+            self.coatingLayerDecoOptions(),
+            self.parameters(),
+        ]
+        if all(x is not None for x in default_invalid_args):
+            expt = self.experimentType()(
+                self.substrate(),
+                self.coatingLayerType(),
+                self.coatingLayerParameters(),
+                self.coatingLayerDrawOptions(),
+                self.coatingLayerDecoOptions(),
+                parameters=self.parameters(),
+            )
+            if not expt.valid():
+                expt = None
+        self._expt = expt
+        self.updateLayerGenerator()
+
+    def experiment(self) -> Optional[ExperimentBase]:
+        """
+        Concrete instance of :class:`ExperimentBase` constructed by experiment
+        object data. ``None`` indicates invalid value.
+
+        Run :meth:`updateExperiment` to update this value.
+        """
+        return self._expt
+
+    @Slot()
+    def updateLayerGenerator(self):
+        expt = self.experiment()
+        if expt is not None:
+            self._layer_generator = expt.layer_generator()
+            next(self.layerGenerator())
+        else:
+            self._layer_generator = None
+
+    def layerGenerator(
+        self,
+    ) -> Generator[CoatingLayerBase, npt.NDArray[np.uint8], None]:
+        """
+        :meth:`Experiment.layer_generator` from :meth:`experiment`.
+        ``None`` indicates invalid value.
+
+        Run :meth:`updateLayerGenerator` to update this value.
+        """
+        return self._layer_generator
+
+    def visualizationMode(self) -> ExperimentVisualizationMode:
+        """If False, analysis result is never visualized."""
+        return self._visualize_mode
+
+    def setVisualizationMode(self, mode: ExperimentVisualizationMode):
+        """
+        Update :meth:`visualizationMode` with *mode*.
+        Also, update :meth:`layerGenerator` if *mode* is True.
+        """
+        self._visualize_mode = mode
+        if mode:
+            self.updateLayerGenerator()
+
+    def image(self) -> npt.NDArray[np.uint8]:
+        """Layer shape image to be visualized."""
+        img = self._img
+        if img is None:
+            img = np.empty((0, 0, 0))
+        return img
+
+    def setImage(self, img: Optional[npt.NDArray[np.uint8]]):
+        """Update :meth:`image` with *img*."""
+        self._img = img
+
+    def visualizedImage(self) -> npt.NDArray[np.uint8]:
+        """
+        Return visualization result of :meth:`image` analyzed by
+        :meth:`experiment`.
+
+        If possible, :meth:`ExperimentBase.layer_generator` is used to construct
+        the visualized image. This implies that consecutively visualized images
+        are consecutive in real world as well. If completely unrelated images
+        should be passed, run :meth:`updateLayerGenerator` first.
+
+        If :meth:`visualizationMode` is ``FULL``, visualization is done by
+        constructing analysis objects. If it is ``FAST``, image is visualized by
+        :meth:`fastVisualize`.
+
+        If parameters are invalid or :meth:`visualizationMode` is ``OFF``,
+        directly return :meth:`image`.
+
+        """
+        ls_gen = self.layerGenerator()
+        vismode = self.visualizationMode()
+        image = self.image()
+        if ls_gen is not None and image.size > 0:
+            if vismode == ExperimentVisualizationMode.FULL:
+                ls = ls_gen.send(image)
+                while ls is None:  # previous construction not finished
+                    ls = ls_gen.send(image)
+                image = ls.draw()
+            elif vismode == ExperimentVisualizationMode.FAST:
+                image = self.fastVisualize()
+            else:
+                pass
+        return image
+
+    def fastVisualize(self) -> npt.NDArray[np.uint8]:
+        """
+        Remove substrate from :meth:`image` without constructing analysis
+        objects.
+
+        This visualization lacks the versatility of full visualization, but it is
+        considerably faster for quick overview.
+        """
+        substrate = self.substrate()
+        if substrate is None:
+            return self.image()
+
+        x0, y0, x1, y1 = substrate.reference.templateROI
+        template = substrate.reference.image[y0:y1, x0:x1]
+        res = cv2.matchTemplate(self.image(), template, cv2.TM_CCOEFF)
+        _, _, _, (tx, ty) = cv2.minMaxLoc(res)
+        dx, dy = substrate.reference.temp2subst()
+        x0, y0 = (tx + dx, ty + dy)
+        subst_h, subst_w = substrate.image().shape[:2]
+        img_h, img_w = self.image().shape[:2]
+        x1, y1 = (x0 + subst_w, y0 + subst_h)
+
+        _, bin_img = cv2.threshold(
+            self.image(), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
+        )
+        bin_img_cropped = bin_img[
+            max(y0, 0) : min(y1, img_h), max(x0, 0) : min(x1, img_w)
+        ]
+        subst_cropped = substrate.image()[
+            max(-y0, 0) : min(img_h - y0, subst_h),
+            max(-x0, 0) : min(img_w - x0, subst_w),
+        ]
+        _, bin_subst_cropped = cv2.threshold(
+            subst_cropped, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
+        )
+
+        xor = cv2.bitwise_xor(bin_img_cropped, bin_subst_cropped)
+        nxor = cv2.bitwise_not(xor)
+        bin_img[max(y0, 0) : min(y1, img_h), max(x0, 0) : min(x1, img_w)] = nxor
+        return cv2.cvtColor(bin_img, cv2.COLOR_GRAY2RGB)
+
+    def emitImage(self):
+        """
+        Emit the result of :meth:`visualizedImage` to
+        :attr:`visualizedImageChanged` signal. If visualization raises error,
+        directly emit :meth:`image`.
+        """
+        self.visualizedImageChanged.emit(self.visualizedImage())
+
+    def clear(self):
+        """
+        Initialize experiment object data, :meth:`experiment`,
+        :meth:`image` and :meth:`paths`.
+        """
+        self.initArgs()
