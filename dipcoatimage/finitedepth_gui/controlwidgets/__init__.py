0f8cd14d1d1c91e176d49104fd370bcdbc4c2b27
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-05-22 01:37:57 +0900

    maint: make gui modules as subpackage
    Make controlwidgets.py as subpackage

---
+++ dipcoatimage/finitedepth_gui/controlwidgets/__init__.py
@@ -0,0 +1,28 @@
+"""
+Control Widgets
+===============
+
+This module provides widgets to control the analysis parameters.
+
+"""
+
+from .base import ControlWidget
+from .exptwidget import ExperimentWidget
+from .refwidget import ReferenceWidget
+from .substwidget import SubstrateWidget
+from .layerwidget import CoatingLayerWidget
+from .analysiswidget import AnalysisWidget
+from .controlwidgets import (
+    MasterControlWidget,
+)
+
+__all__ = [
+    "ControlWidget",
+    "ExperimentWidget",
+    "ReferenceWidget",
+    "SubstrateWidget",
+    "CoatingLayerWidget",
+    "EmptyDoubleValidator",
+    "AnalysisWidget",
+    "MasterControlWidget",
+]
---
+++ dipcoatimage/finitedepth_gui/controlwidgets/base.py
@@ -0,0 +1,32 @@
+from dipcoatimage.finitedepth_gui.inventory import ExperimentItemModel
+from PySide6.QtWidgets import QWidget
+
+
+__all__ = [
+    "ControlWidget",
+]
+
+
+class ControlWidget(QWidget):
+    """Base class for all control widgets."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self._exptitem_model = ExperimentItemModel()
+        self._currentExperimentRow = -1
+
+    def experimentItemModel(self) -> ExperimentItemModel:
+        """Model which holds the experiment item data."""
+        return self._exptitem_model
+
+    def currentExperimentRow(self) -> int:
+        """Currently activated row from :meth:`experimentItemModel`."""
+        return self._currentExperimentRow
+
+    def setExperimentItemModel(self, model: ExperimentItemModel):
+        """Set :meth:`experimentItemModel`."""
+        self._exptitem_model = model
+
+    def setCurrentExperimentRow(self, row: int):
+        self._currentExperimentRow = row
