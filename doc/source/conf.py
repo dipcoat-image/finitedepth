dfd64efb9df854aec594a7a6d0c5e9edfbf08724
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-09-06 10:22:54 +0900

    fix: fix ci
    Remove mock import

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-autodoc_mock_imports = ["PySide6.QtMultimedia"]
-
 
 # -- Project information -----------------------------------------------------
 
