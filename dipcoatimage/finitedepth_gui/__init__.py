2639d94bddddad63435515338705c8b7299fe8e8
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-10-17 12:45:13 +0900

    feat: add mainwindow.py

--- dipcoatimage/finitedepth_gui/__init__.py
+++ dipcoatimage/finitedepth_gui/__init__.py
@@ -5,6 +5,10 @@ GUI package for :mod:`dipcoatimage.finitedepth`.
 from dipcoatimage.finitedepth import __version__  # noqa
 
 from .analysisgui import AnalysisGUI
+from .mainwindow import MainWindow
 
 
-__all__ = ["AnalysisGUI"]
+__all__ = [
+    "AnalysisGUI",
+    "MainWindow",
+]
