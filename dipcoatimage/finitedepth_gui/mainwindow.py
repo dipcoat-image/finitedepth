e2a9ecf307575bcee36f2b52162eea5580367834
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-10-18 10:30:31 +0900

    feat: implement DataViewTab
    Use DataViewTab in MainWindow

--- dipcoatimage/finitedepth_gui/mainwindow.py
+++ dipcoatimage/finitedepth_gui/mainwindow.py
@@ -6,21 +6,9 @@ V2 for analysisgui.py
 """
 
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (
-    QMainWindow,
-    QDockWidget,
-    QTabWidget,
-    QScrollArea,
-)
+from PySide6.QtWidgets import QMainWindow, QDockWidget
 from dipcoatimage.finitedepth_gui.model import ExperimentDataModel
-from dipcoatimage.finitedepth_gui.views import (
-    ExperimentListView,
-    ExperimentView,
-    ReferenceView,
-    SubstrateView,
-    CoatingLayerView,
-    AnalysisView,
-)
+from dipcoatimage.finitedepth_gui.views import ExperimentListView, DataViewTab
 
 
 __all__ = ["MainWindow"]
@@ -46,44 +34,15 @@ class MainWindow(QMainWindow):
 
         self._model = ExperimentDataModel()
         self._listView = ExperimentListView()
-        self._exptView = ExperimentView()
-        self._refView = ReferenceView()
-        self._substView = SubstrateView()
-        self._layerView = CoatingLayerView()
-        self._analysisView = AnalysisView()
+        self._dataViewTab = DataViewTab()
 
         self._listView.setModel(self._model)
-        self._exptView.setModel(self._model)
-        self._refView.setModel(self._model)
-        self._substView.setModel(self._model)
-        self._layerView.setModel(self._model)
-        self._analysisView.setModel(self._model)
+        self._dataViewTab.setModel(self._model)
 
         exptListDock = QDockWidget("List of experiments")
         exptListDock.setWidget(self._listView)
         self.addDockWidget(Qt.LeftDockWidgetArea, exptListDock)
         exptDataDock = QDockWidget("Experiment data")
-        exptDataTabWidget = QTabWidget()
-        exptScroll = QScrollArea()
-        exptScroll.setWidgetResizable(True)
-        exptScroll.setWidget(self._exptView)
-        exptDataTabWidget.addTab(exptScroll, "Experiment")
-        refScroll = QScrollArea()
-        refScroll.setWidgetResizable(True)
-        refScroll.setWidget(self._refView)
-        exptDataTabWidget.addTab(refScroll, "Reference")
-        substScroll = QScrollArea()
-        substScroll.setWidgetResizable(True)
-        substScroll.setWidget(self._substView)
-        exptDataTabWidget.addTab(substScroll, "Substrate")
-        layerScroll = QScrollArea()
-        layerScroll.setWidgetResizable(True)
-        layerScroll.setWidget(self._layerView)
-        exptDataTabWidget.addTab(layerScroll, "Coating layer")
-        analysisScroll = QScrollArea()
-        analysisScroll.setWidgetResizable(True)
-        analysisScroll.setWidget(self._analysisView)
-        exptDataTabWidget.addTab(analysisScroll, "Analysis")
-        exptDataDock.setWidget(exptDataTabWidget)
+        exptDataDock.setWidget(self._dataViewTab)
         self.addDockWidget(Qt.BottomDockWidgetArea, exptDataDock)
         self.setWindowTitle("Coating layer analysis")
