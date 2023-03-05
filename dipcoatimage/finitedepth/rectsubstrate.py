50000ae318c7c3e6847fa6ad80ad9eb461bbd72d
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-03-05 23:32:12 +0900

    refact: move intrsct_pt_polar (util -> rectsubstrate)

--- dipcoatimage/finitedepth/rectsubstrate.py
+++ dipcoatimage/finitedepth/rectsubstrate.py
@@ -47,11 +47,11 @@ import dataclasses
 import enum
 from math import isclose
 import numpy as np
+from numpy.linalg import inv
 import numpy.typing as npt
 from typing import TypeVar, Tuple, Optional, Dict, Type
 from .substrate import SubstrateError, SubstrateBase
 from .util import (
-    intrsct_pt_polar,
     HoughLinesParameters,
     DataclassProtocol,
 )
@@ -73,6 +73,7 @@ __all__ = [
     "RectSubstrateDrawMode",
     "RectSubstrateDrawOptions",
     "RectSubstrate",
+    "intrsct_pt_polar",
 ]
 
 
@@ -517,3 +518,38 @@ class RectSubstrate(
                 cv2.line(ret, bottomleft, topleft, color, thickness)
 
         return ret
+
+
+def intrsct_pt_polar(r1: float, t1: float, r2: float, t2: float) -> Tuple[float, float]:
+    """
+    Find the Cartesian coordinates of the intersecting point of two
+    lines by their polar parameters.
+
+    Parameters
+    ==========
+
+    r1, t1, r2, t2
+        Radius and angle for the first and second line.
+
+    Returns
+    =======
+
+    x, y
+        Cartesian coordinates of the intersecting point.
+
+    Examples
+    ========
+
+    >>> from dipcoatimage.finitedepth.rectsubstrate import intrsct_pt_polar
+    >>> from numpy import pi
+    >>> x, y = intrsct_pt_polar(10, pi/3, 5, pi/6)
+    >>> round(x, 2)
+    -1.34
+    >>> round(y, 2)
+    12.32
+
+    """
+    mat = np.array([[np.cos(t1), np.sin(t1)], [np.cos(t2), np.sin(t2)]])
+    vec = np.array([r1, r2])
+    ret = inv(mat) @ vec
+    return tuple(float(i) for i in ret)  # type: ignore
