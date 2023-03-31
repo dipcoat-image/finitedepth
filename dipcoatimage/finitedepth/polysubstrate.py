30e649a11fb11bd7632dc1ff55bec78f4efa5562
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-03-31 17:30:57 +0900

    refact: implement PolySubstrateBase
    Add corners()

--- dipcoatimage/finitedepth/polysubstrate.py
+++ dipcoatimage/finitedepth/polysubstrate.py
@@ -71,6 +71,8 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
     """
 
     __slots__ = (
+        "_contour",
+        "_corners",
         "_sides",
         "_vertex_points",
     )
@@ -80,25 +82,43 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
     SidesNum: int
 
     def contour(self) -> npt.NDArray[np.int32]:
+        if hasattr(self, "_contour"):
+            return self._contour  # type: ignore[has-type]
+
         (cnt,), _ = cv2.findContours(
             cv2.bitwise_not(self.binary_image()),
             cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_NONE,
         )
-        return cnt
+        self._contour = cnt
+        return self._contour
 
-    def sides(self) -> Tuple[npt.NDArray[np.int64], npt.NDArray[np.float32]]:
-        """Return the contour indices and polar parameters for linear sides."""
-        if hasattr(self, "_sides"):
-            return self._sides  # type: ignore[has-type]
+    def corners(self) -> npt.NDArray[np.int64]:
+        """
+        Return the indices of the corner points on contour.
 
-        contour = self.contour()
+        This method returns the indices of the local maxima of the changes in the
+        direction of the tangent vector. To get the entire corner region of
+        smooth polygon, split the contour using the indices from :meth:`sides`.
+
+        See Also
+        ========
+
+        vertex_points
+            Return coordinates of intersections of sides.
+
+        sides
+            Detects the indices for linear side region.
+
+        """
+        if hasattr(self, "_corners"):
+            return self._corners  # type: ignore[has-type]
 
         # 1. Get theta values of the tangent curve and take smooth derivative.
         # This allows us to find sides even from jittery polygons.
         # Since the contour is periodic and edge information might be lost,
         # we repeat theta in both direction.
-        tan = np.gradient(contour, axis=0)
+        tan = np.gradient(self.contour(), axis=0)
         theta = np.arctan2(tan[..., 1], tan[..., 0])
         theta2 = np.concatenate(
             [
@@ -108,8 +128,7 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
             ],
             axis=0,
         )
-        sigma = self.parameters.GaussianSigma
-        theta2_smooth = gaussian_filter1d(theta2, sigma, axis=0)
+        theta2_smooth = gaussian_filter1d(theta2, self.parameters.GaussianSigma, axis=0)
 
         # 2. Find peak. Each peak shows the point where side changes. This allows
         # us to discern individual sides lying on same line.
@@ -122,27 +141,41 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
         prom, _, _ = peak_prominences(theta2_abs, peaks)
         if len(prom) < self.SidesNum:
             msg = (
-                "Insufficient number of sides"
+                "Insufficient number of corners"
                 f" (needs {self.SidesNum}, detected {len(prom)})"
             )
             raise PolySubstrateError(msg)
         prom_peaks = peaks[np.sort(np.argsort(prom)[-self.SidesNum :])]
-        corner_peaks = np.sort(prom_peaks) - (len(theta) // 2)
+        corners = np.sort(prom_peaks) - (len(theta) // 2)
 
-        # 3. Digitize smoothed line and get votes to determine main theta.
+        self._corners = corners
+        return self._corners
+
+    def sides(self) -> Tuple[npt.NDArray[np.int64], npt.NDArray[np.float32]]:
+        """Return the contour indices and polar parameters for linear sides."""
+        if hasattr(self, "_sides"):
+            return self._sides  # type: ignore[has-type]
+
+        tan = np.gradient(self.contour(), axis=0)
+        theta = np.arctan2(tan[..., 1], tan[..., 0])
+        corners = self.corners()
+
+        # Digitize smoothed line and get votes to determine main theta.
         # We must take vote from disgitized smoothed line, not from raw theta
         # in order to be robust from jittery noises.
         # We smooth each line separately to minimize the effect on shap corner.
-        SHIFT = corner_peaks[0]
-        # roll s.t. no residual section at the beginning
+        SHIFT = corners[0]
+        # roll s.t. no section is divided by the boundary
         theta_roll = np.roll(theta, -SHIFT, axis=0)
-        corner_peaks = corner_peaks - SHIFT
+        corners = corners - SHIFT
 
         THETA_STEP = self.parameters.Theta
         indices = []
         thetas = []
-        for region in np.split(theta_roll, corner_peaks[1:], axis=0):
-            region_smooth = gaussian_filter1d(region, sigma, axis=0)
+        for region in np.split(theta_roll, corners[1:], axis=0):
+            region_smooth = gaussian_filter1d(
+                region, self.parameters.GaussianSigma, axis=0
+            )
             digitized = (region_smooth / THETA_STEP).astype(int) * THETA_STEP
             val, count = np.unique(digitized, return_counts=True)
             main_theta = val[np.argmax(count)]
@@ -150,7 +183,7 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
             indices.append([idxs[0], idxs[-1]])
             thetas.append(main_theta)
 
-        base_indices = SHIFT + corner_peaks[..., np.newaxis]
+        base_indices = SHIFT + corners[..., np.newaxis]
         split_indices = (base_indices + np.array(indices, dtype=np.int64)) % len(theta)
         sortidx = np.argsort(split_indices, axis=0)
         split_indices = split_indices[sortidx[..., 0]]
@@ -158,7 +191,7 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
         # convert slope theta to polar angle (just as HoughLines parameter)
         thetas_array = (np.array(thetas) - np.pi / 2) % np.pi
         angles = thetas_array[sortidx[..., 0]][..., np.newaxis]
-        lines = [contour[i0:i1] for (i0, i1) in split_indices]
+        lines = [self.contour()[i0:i1] for (i0, i1) in split_indices]
         line_centers = np.array([np.mean(line, axis=0) for line in lines])
 
         x_cos = line_centers[..., 0] * np.cos(angles)
@@ -170,6 +203,19 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
         return self._sides
 
     def vertex_points(self):
+        """
+        Return the coordinates of intersections of :meth:`sides`.
+
+        If the polygon has smooth corner, the vertex points are different from
+        the corner points on the contour.
+
+        See Also
+        ========
+
+        corners
+            Return indices of the corner points on contour.
+
+        """
         if hasattr(self, "_vertex_points"):
             return self._vertex_points  # type: ignore[has-type]
 
@@ -189,7 +235,7 @@ class PolySubstrateBase(SubstrateBase[ParametersType, DrawOptionsType]):
 
     def examine(self) -> Optional[PolySubstrateError]:
         try:
-            self.sides()
+            self.corners()
         except PolySubstrateError as err:
             return err
         return None
