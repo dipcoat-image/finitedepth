54a68ba2002f1f0d81d0fa6c7a953bb00e8d1cd5
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-04-13 13:07:34 +0900

    feat: implement roughness measure by frechet distance
    Rename df to dfd
    Make dfd return accumulated cost array
    Implement dfd_pair

--- dipcoatimage/finitedepth/util/frechet.py
+++ dipcoatimage/finitedepth/util/frechet.py
@@ -2,17 +2,19 @@ import numpy as np
 import numpy.typing as npt
 from numba import njit  # type: ignore
 from scipy.spatial.distance import cdist  # type: ignore
+from typing import Tuple
 
 
 __all__ = [
-    "df",
+    "dfd",
+    "dfd_pair",
 ]
 
 
-def df(
+def dfd(
     P: npt.NDArray[np.float64],
     Q: npt.NDArray[np.float64],
-) -> np.float64:
+) -> npt.NDArray[np.float64]:
     """
     Compute discrete Fréchet distance with Euclidean metric.
 
@@ -26,9 +28,10 @@ def df(
 
     Returns
     -------
-    np.float64
-        Discrete Fréchet distance value.
-        If *P* or *Q* is empty, return value is zero.
+    ca: ndarray
+        Accumulated cost array for the Fréchet distance.
+        The Fréchet distance is the last element of the array i.e. `ca[-1, -1]`.
+        If *P* or *Q* is empty, return value is an empty array.
 
     References
     ----------
@@ -38,31 +41,52 @@ def df(
     .. [2] https://pypi.org/project/similaritymeasures/
 
     """
-    freespace = cdist(P, Q)
-    return _df(freespace)
+    # TODO: use subquadratic algorithm (Agarwal et al, 2014)
+    return _dfd(cdist(P, Q))
 
 
 @njit
-def _df(fs: npt.NDArray[np.float64]) -> np.float64:
-    p, q = fs.shape
-    if p == 0 or q == 0:
-        return np.float64(0)
-
+def _dfd(freespace: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    p, q = freespace.shape
     ca = np.zeros((p, q), dtype=np.float64)
+    if p == 0 or q == 0:
+        return ca
     ca.fill(-1.0)
 
-    ca[0, 0] = np.sqrt(fs[0, 0])
+    ca[0, 0] = np.sqrt(freespace[0, 0])
 
     for i in range(1, p):
-        ca[i, 0] = max(ca[i - 1, 0], fs[i, 0])
+        ca[i, 0] = max(ca[i - 1, 0], freespace[i, 0])
 
     for j in range(1, q):
-        ca[0, j] = max(ca[0, j - 1], fs[0, j])
+        ca[0, j] = max(ca[0, j - 1], freespace[0, j])
 
     for i in range(1, p):
         for j in range(1, q):
             ca[i, j] = max(
                 min(ca[i - 1, j], ca[i, j - 1], ca[i - 1, j - 1]),
-                fs[i, j],
+                freespace[i, j],
             )
-    return ca[p - 1, q - 1]
+    return ca
+
+
+def dfd_pair(ca: npt.NDArray[np.float64]) -> Tuple[np.int64, np.int64]:
+    """
+    Find the point pair which determines the discrete Fréchet distance.
+
+    Parameters
+    ----------
+    ca: ndarray
+        Accumulated cost array of discrete Fréchet distance between two curves.
+
+    Returns
+    -------
+    tuple
+        Indices for the two curves to get the point pair.
+
+    See Also
+    --------
+    dfd
+    """
+    i, j = np.nonzero(ca == ca[-1, -1])
+    return (i[0], j[0])
