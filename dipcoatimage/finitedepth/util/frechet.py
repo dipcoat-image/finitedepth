7a4b927b958316b797e45c3c1d5a97c85ecbe877
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-07-02 22:51:35 +0900

    fix: fix frechet distances
    Add dynamic time warping
    
    Todo: fix sfd

--- dipcoatimage/finitedepth/util/frechet.py
+++ dipcoatimage/finitedepth/util/frechet.py
@@ -8,6 +8,8 @@ from typing import Tuple
 __all__ = [
     "dfd",
     "dfd_pair",
+    "dtw",
+    "dtw_path",
     "sfd",
     "sfd_path",
     "ssfd",
@@ -96,13 +98,137 @@ def dfd_pair(ca: npt.NDArray[np.float64]) -> npt.NDArray[np.int32]:
     return np.array([[i[0], j[0]]], dtype=np.int32)
 
 
+def dtw(
+    P: npt.NDArray[np.float64],
+    Q: npt.NDArray[np.float64],
+) -> npt.NDArray[np.float64]:
+    """
+    Compute dynamic time warping[1]_ with L2 norm.
+
+    Implements the algorithm from [1]_, with modification from [2]_.
+
+    Parameters
+    ----------
+    P, Q : ndarray
+        2D Numpy array with ``np.float64`` dtype. Rows are the points and
+        columns are the dimensions.
+
+    Returns
+    -------
+    ndarray
+        Accumulated cost array for dynamic time warping.
+        The distance is the last element of the array i.e. `ca[-1, -1]`.
+        If *P* or *Q* is empty, return value is an empty array.
+
+    References
+    ----------
+    .. [1] Senin, Pavel. "Dynamic time warping algorithm review." Information and
+       Computer Science Department University of Hawaii at Manoa Honolulu,
+       USA 855.1-23 (2008): 40.
+
+    .. [2] https://pypi.org/project/similaritymeasures/
+
+    """
+    return _dtw(cdist(P, Q))
+
+
+@njit(cache=True)
+def _dtw(freespace: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    p, q = freespace.shape
+    ca = np.zeros((p, q), dtype=np.float64)
+    if p == 0 or q == 0:
+        return ca
+
+    ca[0, 0] = freespace[0, 0]
+
+    for i in range(1, p):
+        ca[i, 0] = ca[i - 1, 0] + freespace[i, 0]
+
+    for j in range(1, q):
+        ca[0, j] = ca[0, j - 1] + freespace[0, j]
+
+    for i in range(1, p):
+        for j in range(1, q):
+            ca[i, j] = (
+                min(ca[i - 1, j], ca[i, j - 1], ca[i - 1, j - 1]) + freespace[i, j]
+            )
+
+    return ca
+
+
+def dtw_path(ca: npt.NDArray[np.float64]) -> npt.NDArray[np.int32]:
+    """
+    Compute optimal path for dynamic time warping in the free space.
+
+    Implements the algorithm from [1]_, with modification from [2]_.
+
+    Parameters
+    ----------
+    ca: ndarray
+        Accumulated cost array of dynamic time warping between two curves.
+
+    Returns
+    -------
+    ndarray
+        Indices for the two curves to get the path.
+
+    References
+    ----------
+    .. [1] Senin, Pavel. "Dynamic time warping algorithm review." Information and
+       Computer Science Department University of Hawaii at Manoa Honolulu,
+       USA 855.1-23 (2008): 40.
+
+    .. [2] https://pypi.org/project/similaritymeasures/
+
+    See Also
+    --------
+    dtw
+
+    """
+    path, path_len = _dtw_path(ca)
+    return path[-(len(path) - path_len + 1) :: -1]
+
+
+@njit(cache=True)
+def _dtw_path(ca: npt.NDArray[np.float64]) -> Tuple[npt.NDArray[np.int32], np.int32]:
+    p, q = ca.shape
+    if p == 0 or q == 0:
+        return np.empty((0, 2), dtype=np.int32), np.int32(0)
+
+    path = np.zeros((p + q - 1, 2), dtype=np.int32)
+    path_len = np.int32(0)
+
+    i, j = p - 1, q - 1
+    path[path_len] = [i, j]
+    path_len += 1
+
+    while i > 0 or j > 0:
+        if i == 0:
+            j -= 1
+        elif j == 0:
+            i -= 1
+        else:
+            d = min(ca[i - 1, j], ca[i, j - 1], ca[i - 1, j - 1])
+            if ca[i - 1, j] == d:
+                i -= 1
+            elif ca[i, j - 1] == d:
+                j -= 1
+            else:
+                i -= 1
+                j -= 1
+
+        path[path_len] = [i, j]
+        path_len += 1
+
+    return path, path_len
+
+
 def sfd(
     P: npt.NDArray[np.float64],
     Q: npt.NDArray[np.float64],
 ) -> npt.NDArray[np.float64]:
     """
-    Compute summed Fréchet distance (equivalent to dynamic time warping) with
-    Euclidean metric.
+    Compute summed Fréchet distance with Euclidean metric.
 
     Implements the algorithm from [1]_, with modification from [2]_.
 
@@ -128,29 +254,28 @@ def sfd(
     .. [2] https://pypi.org/project/similaritymeasures/
 
     """
-    return _sfd(cdist(P, Q), 1)
+    return _sfd(cdist(P, Q))
 
 
 @njit(cache=True)
-def _sfd(freespace: npt.NDArray[np.float64], order: int) -> npt.NDArray[np.float64]:
+def _sfd(freespace: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     p, q = freespace.shape
     ca = np.zeros((p, q), dtype=np.float64)
     if p == 0 or q == 0:
         return ca
 
-    ca[0, 0] = freespace[0, 0] ** order
+    ca[0, 0] = freespace[0, 0]
 
     for i in range(1, p):
-        ca[i, 0] = ca[i - 1, 0] + freespace[i, 0] ** order
+        ca[i, 0] = ca[i - 1, 0] + freespace[i, 0]
 
     for j in range(1, q):
-        ca[0, j] = ca[0, j - 1] + freespace[0, j] ** order
+        ca[0, j] = ca[0, j - 1] + freespace[0, j]
 
     for i in range(1, p):
         for j in range(1, q):
             ca[i, j] = (
-                min(ca[i - 1, j], ca[i, j - 1], ca[i - 1, j - 1])
-                + freespace[i, j] ** order
+                min(ca[i - 1, j], ca[i, j - 1], ca[i - 1, j - 1]) + freespace[i, j]
             )
 
     return ca
