25578674a5cfb22ef283dd30b22246ccd1ee6e14
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-09-25 15:28:10 +0900

    refact: make ExperimentBase perform template matching
    Revert Experiment parameters

--- src/dipcoatimage/finitedepth/experiment_param.py
+++ src/dipcoatimage/finitedepth/experiment_param.py
@@ -8,14 +8,4 @@ __all__ = [
 
 @dataclasses.dataclass(frozen=True)
 class Parameters:
-    """
-    Additional parameters for :class:`Experiment` instance.
-
-    Attributes
-    ----------
-    fast : bool
-        If True, optimization is used to speed up template matching.
-        If False, brute force method is used (slow but reliable).
-    """
-
-    fast: bool = True
+    """Additional parameters for :class:`Experiment` instance."""
